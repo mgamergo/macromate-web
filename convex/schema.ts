6863b6cb99ccb349541450754c84ea239bf5c403
Chaitanya Swaroop <chaitanyaswaroop36@gmail.com>
2026-01-07 00:01:14 +0530

    Feat: Created schema for users. steps, meals

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,8 +5,30 @@ export default defineSchema({
     users: defineTable({
         name: v.string(),
         email: v.string(),
-        tokenIdentifier: v.string(),
+        clerkUserId: v.string(),
         pictureUrl: v.optional(v.string()),
-    }).index("by_token", ["tokenIdentifier"]),
+
+        createdAt: v.number(),
+    }).index("by_clerk_id", ["clerkUserId"]),
+
+    meals: defineTable({
+        userId: v.id("users"),
+        name: v.string(),
+        calories: v.number(),
+        protein: v.number(),
+        carbs: v.number(),
+        fats: v.number(),
+        type: v.union(v.literal("breakfast"), v.literal("lunch"), v.literal("dinner"), v.literal("snack")),
+
+        createdAt: v.number(),
+    }).index("by_user", ["userId"]),
+
+    steps: defineTable({
+        userId: v.id("users"),
+        stepCount: v.number(),
+
+        createdAt: v.number(),
+    }).index("by_user", ["userId"])
+    .index("by_user_createdAt", ["userId", "createdAt"]),
 });
 
